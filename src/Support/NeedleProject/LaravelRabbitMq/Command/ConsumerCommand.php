cc9f57dd2619fa2338df0ff3e0ce61a870d5c335
smskin <serg@msoinvest.com>
2022-06-06 13:02:45 +0300

    fixed structure

--- src/Support/NeedleProject/LaravelRabbitMq/Command/ConsumerCommand.php
+++ src/Support/NeedleProject/LaravelRabbitMq/Command/ConsumerCommand.php
@@ -3,8 +3,12 @@
 namespace SMSkin\ServiceBus\Support\NeedleProject\LaravelRabbitMq\Command;
 
 use NeedleProject\LaravelRabbitMq\Command\BaseConsumerCommand;
+use Psr\Log\LoggerAwareInterface;
 use SMSkin\ServiceBus\Support\NeedleProject\LaravelRabbitMq\ConsumerInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Symfony\Component\Console\Command\SignalableCommandInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 /**
  * Class BaseConsumerCommand
@@ -12,7 +16,7 @@ use Illuminate\Contracts\Container\BindingResolutionException;
  * @package NeedleProject\LaravelRabbitMq\Command
  * @author  Adrian Tilita <adrian@tilita.ro>
  */
-class ConsumerCommand extends BaseConsumerCommand
+class ConsumerCommand extends BaseConsumerCommand implements SignalableCommandInterface
 {
     /**
      * The name and signature of the console command.
@@ -20,6 +24,40 @@ class ConsumerCommand extends BaseConsumerCommand
      * @var string
      */
     protected $signature = 'service-bus:consume {consumer} {--time=60} {--messages=100} {--memory=64}';
+
+    protected ConsumerInterface $consumer;
+
+    /**
+     * Execute the console command.
+     * @return int
+     * @throws BindingResolutionException
+     */
+    public function handle(): int
+    {
+        $messageCount = $this->input->getOption('messages');
+        $waitTime = $this->input->getOption('time');
+        $memoryLimit = $this->input->getOption('memory');
+        $isVerbose = in_array(
+            $this->output->getVerbosity(),
+            [OutputInterface::VERBOSITY_VERBOSE, OutputInterface::VERBOSITY_VERY_VERBOSE]
+        );
+
+        $this->consumer = $consumer = $this->getConsumer($this->input->getArgument('consumer'));
+        if ($consumer instanceof LoggerAwareInterface && $isVerbose) {
+            try {
+                $this->injectCliLogger($consumer);
+            } catch (Throwable $e) {
+                // Do nothing, we cannot inject a STDOUT logger
+            }
+        }
+        try {
+            return $consumer->startConsuming($messageCount, $waitTime, $memoryLimit);
+        } catch (Throwable $e) {
+            $consumer->stopConsuming();
+            $this->output->error($e->getMessage());
+            return -1;
+        }
+    }
 
     /**
      * @param string $consumerAliasName
@@ -29,5 +67,28 @@ class ConsumerCommand extends BaseConsumerCommand
     protected function getConsumer(string $consumerAliasName): ConsumerInterface
     {
         return app()->make(ConsumerInterface::class, [$consumerAliasName]);
+    }
+
+    /**
+     * Get the list of signals handled by the command.
+     *
+     * @return array
+     */
+    public function getSubscribedSignals(): array
+    {
+        return [SIGINT, SIGTERM];
+    }
+
+    /**
+     * Handle an incoming signal.
+     *
+     * @param int $signal
+     * @return void
+     */
+    public function handleSignal(int $signal): void
+    {
+        if ($signal === SIGINT) {
+            $this->consumer->stopConsuming();
+        }
     }
 }
--- src/Support/NeedleProject/LaravelRabbitMq/Entity/QueueEntity.php
+++ src/Support/NeedleProject/LaravelRabbitMq/Entity/QueueEntity.php
@@ -9,5 +9,20 @@ use Psr\Log\LoggerAwareInterface;
 
 class QueueEntity extends \NeedleProject\LaravelRabbitMq\Entity\QueueEntity implements PublisherInterface, ConsumerInterface, AMQPEntityInterface, LoggerAwareInterface
 {
+    protected bool $working = true;
 
+    protected function shouldStopConsuming(): bool
+    {
+        if (parent::shouldStopConsuming()) {
+            return true;
+        }
+
+        return $this->working;
+    }
+
+    public function stopConsuming()
+    {
+        parent::stopConsuming();
+        $this->working = false;
+    }
 }
