7cccd0998fc6de65e3316a65a7e22cd6057b60d6
bear <webkolesnikov@gmail.com>
2022-09-26 10:41:18 +0200

    priority

--- src/Support/NeedleProject/LaravelRabbitMq/Entity/QueueEntity.php
+++ src/Support/NeedleProject/LaravelRabbitMq/Entity/QueueEntity.php
@@ -45,6 +45,34 @@ class QueueEntity extends \NeedleProject\LaravelRabbitMq\Entity\QueueEntity impl
         }
     }
 
+    public function bind()
+    {
+        if (!isset($this->attributes['bind']) || empty($this->attributes['bind'])) {
+            return;
+        }
+        foreach ($this->attributes['bind'] as $bindItem) {
+            try {
+                $options = new AMQPTable([
+                    'x-max-priority' => 3,
+                ]);
+                $this->getChannel()
+                    ->queue_bind(
+                        $this->attributes['name'],
+                        $bindItem['exchange'],
+                        $bindItem['routing_key'],
+                        false,
+                        $options,
+                    );
+            } catch (AMQPProtocolChannelException $e) {
+                // 404 is the code for trying to bind to an non-existing entity
+                if (true === $this->attributes['throw_exception_on_bind_fail'] || $e->amqp_reply_code !== 404) {
+                    throw $e;
+                }
+                $this->reconnect();
+            }
+        }
+    }
+
     protected function shouldStopConsuming(): bool
     {
         if (parent::shouldStopConsuming()) {
